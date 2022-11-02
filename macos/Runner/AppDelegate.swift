e6fe2f24c9c66001bd499b838853991897ce6fbc
mitrakov <mitrakov-artem@yandex.ru>
2022-11-03 01:59:31 +0400

    Add opening files on MacOS with Finder

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -6,4 +6,10 @@ class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return true
   }
+
+    // called when a user double-clicks on a file in Finder
+    override func application(_ sender: NSApplication, openFile filename: String) -> Bool {
+        (mainFlutterWindow as! MainFlutterWindow).currentFile = filename
+      return true
+    }
 }
--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -2,14 +2,27 @@ import Cocoa
 import FlutterMacOS
 
 class MainFlutterWindow: NSWindow {
+    open var currentFile: String?
+
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController.init()
-    let windowFrame = self.frame
     self.contentViewController = flutterViewController
-    self.setFrame(windowFrame, display: true)
+    self.setFrame(NSScreen.main?.frame ?? self.frame, display: true) // full-screen
+
+    // interop with Flutter
+    let channel = FlutterMethodChannel(name: "mitrakov", binaryMessenger: flutterViewController.engine.binaryMessenger)
+    channel.setMethodCallHandler({
+        (call: FlutterMethodCall, result: FlutterResult) -> Void in
+        if (call.method == "getCurrentFile") {
+            result(self.currentFile)
+        } else {
+            result(FlutterMethodNotImplemented)
+        }
+    })
 
     RegisterGeneratedPlugins(registry: flutterViewController)
 
+    NSApp.activate(ignoringOtherApps: true) // move window to front
     super.awakeFromNib()
   }
 }
