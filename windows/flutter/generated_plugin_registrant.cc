317266ff8c7737cc0d94cd1c4a5da9d63738bd42
mitrakov <mitrakov-artem@yandex.ru>
2023-02-14 23:27:29 +0400

    Add menubar for Windows and Linux

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,12 +7,15 @@
 #include "generated_plugin_registrant.h"
 
 #include <flutter_platform_alert/flutter_platform_alert_plugin.h>
+#include <menubar/menubar_plugin.h>
 #include <screen_retriever/screen_retriever_plugin.h>
 #include <window_manager/window_manager_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   FlutterPlatformAlertPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FlutterPlatformAlertPlugin"));
+  MenubarPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("MenubarPlugin"));
   ScreenRetrieverPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("ScreenRetrieverPlugin"));
   WindowManagerPluginRegisterWithRegistrar(
