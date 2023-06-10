3adb21f6af07558083864cda69d61c5c203f44d2
mitrakov <mitrakov-artem@yandex.ru>
2023-06-10 16:03:30 +0300

    Add Bulk Rotate operations; Add AVIF; Fix FileDialog extensions

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, curly_braces_in_flow_control_structures, use_build_context_synchronously
+// ignore_for_file: prefer_const_constructors, curly_braces_in_flow_control_structures, use_build_context_synchronously, avoid_function_literals_in_foreach_calls
 import "dart:io";
 import "package:flutter/material.dart";
 import "package:flutter/services.dart";
@@ -21,6 +21,8 @@ void main(List<String> args) async {
   runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Scaffold(body: MyApp(startFile))));
 }
 
+const _allowedExtensions = ["jpg", "jpeg", "png", "gif", "webp", "bmp", "wbmp", "heic", "ico", "cur", "avif"]; // should match the ones in Info.plist!
+
 /// Returns a file that has been opened with our App (or "" if a user cancels OpenFileDialog)
 Future<String> getStartFile(List<String> args) async {
   if (args.isNotEmpty) return args.first;
@@ -30,7 +32,7 @@ Future<String> getStartFile(List<String> args) async {
     final String? currentFile = await hostApi.invokeMethod("getCurrentFile");
     if (currentFile != null) return currentFile;
   }
-  FilePickerResult? result = await FilePicker.platform.pickFiles(dialogTitle: "Select a picture", type: FileType.image); // TODO image: return '"bmp", "gif", "jpeg", "jpg", "png"';
+  FilePickerResult? result = await FilePicker.platform.pickFiles(dialogTitle: "Select a picture", type: FileType.custom, allowedExtensions: _allowedExtensions);
   return result?.files.first.path ?? "";
 }
 
@@ -39,11 +41,10 @@ class MyApp extends StatefulWidget {
   late final List<File> files;
 
   MyApp(this.startPath, {Key? key}) : super(key: key) {
-    final allowedExtensions = {".jpg", ".jpeg", ".png", ".gif", ".webp", ".bmp", ".wbmp"}; // should match the ones in Info.plist!
     files = Directory(path.dirname(startPath))
-      .listSync()                                                                          // get all folder children
-      .whereType<File>()                                                                   // filter out directories
-      .where((f) => allowedExtensions.contains(path.extension(f.path).toLowerCase()))      // filter by extension
+      .listSync()                                                                                        // get all folder children
+      .whereType<File>()                                                                                 // filter out directories
+      .where((f) => _allowedExtensions.contains(path.extension(f.path).toLowerCase().substring(1)))      // filter by extension; substring() removes "."
       .toList()
       ..sort((a, b) => a.path.compareTo(b.path));
   }
@@ -97,6 +98,10 @@ class _MyAppState extends State<MyApp> {
             PlatformMenuItem(label: "Turn ⟲        ↓",              onSelected: _rotateCounterclockwise)
           ]),
           PlatformMenuItemGroup(members: [
+            PlatformMenuItem(label: "Bulk Turn ⟳   ⇧↑",              onSelected: _rotateClockwiseBulk),
+            PlatformMenuItem(label: "Bulk Turn ⟲   ⇧↓",              onSelected: _rotateCounterclockwiseBulk)
+          ]),
+          PlatformMenuItemGroup(members: [
             PlatformMenuItem(label: "Crop            ⌘E or F3",      onSelected: _switchMode)
           ])
         ]),
@@ -110,6 +115,8 @@ class _MyAppState extends State<MyApp> {
           SingleActivator(LogicalKeyboardKey.arrowLeft):                                                PreviousImageIntent(),
           SingleActivator(LogicalKeyboardKey.arrowUp):                                                  RotateClockwiseIntent(),
           SingleActivator(LogicalKeyboardKey.arrowDown):                                                RotateCounterclockwiseIntent(),
+          SingleActivator(LogicalKeyboardKey.arrowUp, shift: true):                                     RotateClockwiseBulkIntent(),
+          SingleActivator(LogicalKeyboardKey.arrowDown, shift: true):                                   RotateCounterclockwiseBulkIntent(),
           SingleActivator(LogicalKeyboardKey.delete):                                                   DeleteFileIntent(),
           SingleActivator(LogicalKeyboardKey.backspace):                                                DeleteFileIntent(),
           SingleActivator(LogicalKeyboardKey.enter):                                                    SaveFileIntent(),
@@ -125,17 +132,19 @@ class _MyAppState extends State<MyApp> {
         },
         child: Actions(
           actions: {
-            NextImageIntent:              CallbackAction(onInvoke: (_) => _nextImage()),
-            PreviousImageIntent:          CallbackAction(onInvoke: (_) => _previousImage()),
-            RotateClockwiseIntent:        CallbackAction(onInvoke: (_) => _rotateClockwise()),
-            RotateCounterclockwiseIntent: CallbackAction(onInvoke: (_) => _rotateCounterclockwise()),
-            DeleteFileIntent:             CallbackAction(onInvoke: (_) => _deleteFile()),
-            SaveFileIntent:               CallbackAction(onInvoke: (_) => _saveFile()),
-            RenameFileIntent:             CallbackAction(onInvoke: (_) => _renameFile(context)),
-            SwitchModeIntent:             CallbackAction(onInvoke: (_) => _switchMode()),
-            SetModeViewerIntent:          CallbackAction(onInvoke: (_) => _setModeToViewer()),
-            AboutDialogIntent:            CallbackAction(onInvoke: (_) => _showAboutDialog()),
-            CloseWindowIntent:            CallbackAction(onInvoke: (_) => exit(0)),
+            NextImageIntent:                  CallbackAction(onInvoke: (_) => _nextImage()),
+            PreviousImageIntent:              CallbackAction(onInvoke: (_) => _previousImage()),
+            RotateClockwiseIntent:            CallbackAction(onInvoke: (_) => _rotateClockwise()),
+            RotateCounterclockwiseIntent:     CallbackAction(onInvoke: (_) => _rotateCounterclockwise()),
+            RotateClockwiseBulkIntent:        CallbackAction(onInvoke: (_) => _rotateClockwiseBulk()),
+            RotateCounterclockwiseBulkIntent: CallbackAction(onInvoke: (_) => _rotateCounterclockwiseBulk()),
+            DeleteFileIntent:                 CallbackAction(onInvoke: (_) => _deleteFile()),
+            SaveFileIntent:                   CallbackAction(onInvoke: (_) => _saveFile()),
+            RenameFileIntent:                 CallbackAction(onInvoke: (_) => _renameFile(context)),
+            SwitchModeIntent:                 CallbackAction(onInvoke: (_) => _switchMode()),
+            SetModeViewerIntent:              CallbackAction(onInvoke: (_) => _setModeToViewer()),
+            AboutDialogIntent:                CallbackAction(onInvoke: (_) => _showAboutDialog()),
+            CloseWindowIntent:                CallbackAction(onInvoke: (_) => exit(0)),
           },
           child: Focus(              // needed for Shortcuts
             autofocus: true,         // focused by default
@@ -219,6 +228,37 @@ class _MyAppState extends State<MyApp> {
     }
   }
 
+  void _rotateClockwiseBulk() => _rotateBulkInternal(true);
+
+  void _rotateCounterclockwiseBulk() => _rotateBulkInternal(false);
+
+  void _rotateBulkInternal(bool clockwise) async {
+    if (_mode == ExtendedImageMode.gesture) {
+      if (!isRotated) {
+        if (isWebp) _showWebpNotSupportedDialog();
+        else {
+          const title = "Attention!";
+          final text = "Are you sure you want to rotate and save ALL ${widget.files.length} file(s) at this folder ${clockwise ? "clockwise" : "counterclockwise"}?";
+          if (await FlutterPlatformAlert.showAlert(windowTitle: title, text: text, alertStyle: AlertButtonStyle.yesNo, iconStyle: IconStyle.warning) == AlertButton.yesButton) {
+            final ImageConverter converter = Platform.isMacOS ? _converterMacOs : _converterWinLinux;
+            final int rotate = clockwise ? 90 : -90;
+            widget.files.forEach((file) async {
+              final Uint8List image = file.readAsBytesSync();
+              final Uint8List bytes = await converter.call(image, file.path, rotate, null);
+              file.writeAsBytesSync(bytes, flush: true);
+              if (file == _currentFile) {
+                setState(() {
+                  _forceLoad = bytes; // force reload current image from disk
+                });
+              }
+            });
+            clearMemoryImageCache();  // clear image cache, this is needed!!!
+          }
+        }
+      }
+    }
+  }
+
   void _deleteFile() async {
     if (_mode == ExtendedImageMode.gesture) {
       const title = "Delete file?";
@@ -269,28 +309,7 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
-  void _saveFile() {
-    // converter that uses "image" library (buggy, not recommended, cross-platform)
-    converterWinLinux(Uint8List image, int? rotate, Rect? cropRect) {
-      final image0 = img.decodeImage(image)!;                                         // use v4.0.11+ (https://github.com/brendan-duncan/image/issues/460)
-      final image1 = rotate == null ? image0 : img.copyRotate(image0, angle: rotate); // use v4.0.12+ (https://github.com/brendan-duncan/image/issues/462)
-      final image2 = cropRect == null ? image1 : img.copyCrop(image1, x: cropRect.left.toInt(), y: cropRect.top.toInt(), width: cropRect.width.toInt(), height: cropRect.height.toInt());
-      return Future.value(img.encodeNamedImage(_currentFile.path, image2)!);
-    }
-    // converter that uses "image_editor" library (recommended, not supported for Windows/Linux)
-    converterMacOs(Uint8List image, int? rotate, Rect? cropRect) async {
-      final option = ImageEditorOption(); // AddTextOption, ClipOption, ColorOption, DrawOption, FlipOption, MaxImageOption, RotateOption, ScaleOption
-      if (rotate != null) option.addOption(RotateOption(rotate));
-      if (cropRect != null) option.addOption(ClipOption(x: cropRect.left, y: cropRect.top, width: cropRect.width, height: cropRect.height));
-      final result = await ImageEditor.editImage(image: image, imageEditorOption: option);
-      return result!;
-    }
-
-    if (Platform.isWindows || Platform.isLinux) _saveFileInternal(converterWinLinux);
-    else _saveFileInternal(converterMacOs);
-  }
-
-  void _saveFileInternal(ImageConverter converter) async {
+  void _saveFile() async {
     int? rotateOption;
     Rect? cropOption;
 
@@ -312,12 +331,13 @@ class _MyAppState extends State<MyApp> {
     }
 
     if (rotateOption != null || cropOption != null) {
+      final ImageConverter converter = Platform.isMacOS ? _converterMacOs : _converterWinLinux;
       final widget = extImgKey.currentWidget as ExtendedImage;         // editorKey cannot be used here!
       final imageProvider = widget.image as ExtendedFileImageProvider; // now it's always ExtendedFileImageProvider, but theoretically might be ExtendedMemoryImageProvider
       final Uint8List image = imageProvider.rawImageData;
-      final Uint8List bytes = await converter.call(image, rotateOption, cropOption);
+      final Uint8List bytes = await converter.call(image, _currentFile.path, rotateOption, cropOption);
       _currentFile.writeAsBytesSync(bytes, flush: true);
-      clearMemoryImageCache(); // clear image cache
+      clearMemoryImageCache(); // clear image cache, this is needed!!!
       setState(() {
         _forceLoad = bytes;    // force reload current image from disk
         _setModeToViewer();
@@ -334,6 +354,23 @@ class _MyAppState extends State<MyApp> {
     final info = await PackageInfo.fromPlatform();
     final text = "v${info.version} (build: ${info.buildNumber})\n\n© Artem Mitrakov. All rights reserved\nmitrakov-artem@yandex.ru";
     FlutterPlatformAlert.showAlert(windowTitle: info.appName, text: text, iconStyle: IconStyle.information);
+  }
+
+  /// converter that uses "image" library (buggy, not recommended, but cross-platform)
+  Future<Uint8List> _converterWinLinux(Uint8List image, String path, int? rotate, Rect? cropRect) {
+    final image0 = img.decodeImage(image)!;                                         // use v4.0.11+ (https://github.com/brendan-duncan/image/issues/460)
+    final image1 = rotate == null ? image0 : img.copyRotate(image0, angle: rotate); // use v4.0.12+ (https://github.com/brendan-duncan/image/issues/462)
+    final image2 = cropRect == null ? image1 : img.copyCrop(image1, x: cropRect.left.toInt(), y: cropRect.top.toInt(), width: cropRect.width.toInt(), height: cropRect.height.toInt());
+    return Future.value(img.encodeNamedImage(path, image2)!);
+  }
+
+  /// converter that uses "image_editor" library (recommended, more stable, but not supported for Windows/Linux);
+  Future<Uint8List> _converterMacOs(Uint8List image, String path, int? rotate, Rect? cropRect) async {
+    final option = ImageEditorOption(); // AddTextOption, ClipOption, ColorOption, DrawOption, FlipOption, MaxImageOption, RotateOption, ScaleOption
+    if (rotate != null) option.addOption(RotateOption(rotate));
+    if (cropRect != null) option.addOption(ClipOption(x: cropRect.left, y: cropRect.top, width: cropRect.width, height: cropRect.height));
+    final result = await ImageEditor.editImage(image: image, imageEditorOption: option);
+    return result!;
   }
 
   String? _validateFilename(String? s) {
@@ -370,6 +407,9 @@ class _MyAppState extends State<MyApp> {
         NativeMenuItem(label: "Turn ⟳        ↑",                       onSelected: _rotateClockwise),
         NativeMenuItem(label: "Turn ⟲        ↓",                       onSelected: _rotateCounterclockwise),
         const NativeMenuDivider(),
+        NativeMenuItem(label: "Bulk Turn ⟳   Shift+↑",                  onSelected: _rotateClockwiseBulk),
+        NativeMenuItem(label: "Bulk Turn ⟲   Shift+↓",                  onSelected: _rotateCounterclockwiseBulk),
+        const NativeMenuDivider(),
         NativeMenuItem(label: "Crop            Ctrl+E or F3",           onSelected: _switchMode)
       ]),
       NativeSubmenu(label: "Help", children: [
@@ -392,13 +432,15 @@ class _MyAppState extends State<MyApp> {
 }
 
 // Typedefs
-typedef ImageConverter = Future<Uint8List> Function(Uint8List curImage, int? rotate, Rect? cropRect);
+typedef ImageConverter = Future<Uint8List> Function(Uint8List curImage, String path, int? rotate, Rect? cropRect);
 
 // Hotkey intents
 class NextImageIntent extends Intent {}
 class PreviousImageIntent extends Intent {}
 class RotateClockwiseIntent extends Intent {}
 class RotateCounterclockwiseIntent extends Intent {}
+class RotateClockwiseBulkIntent extends Intent {}
+class RotateCounterclockwiseBulkIntent extends Intent {}
 class SaveFileIntent extends Intent {}
 class RenameFileIntent extends Intent {}
 class DeleteFileIntent extends Intent {}
